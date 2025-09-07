3f3d8508d6d700ed7a5e85312d27477182c6fd70
Ngoc Khanh <ngockhanh7643@gmail.com>
2025-09-08 00:16:38 +0700

    fix: deployment problem

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,24 @@
 export interface Recipe {
   id: number;
-  image: string,
-  title: string,
-  author: string,   
-  description: string,
-  longDescription: string,
-  cookingTime: string,
-  category: string[],
-  ingredients: string[],
-  steps: string[],
-  isFavourite?: boolean,
-  rating: number,
-  comments?: Comment[]
-  createdAt: string
+  image: string;
+  title: string;
+  author: string;
+  description: string;
+  longDescription: string;
+  cookingTime: string;
+  category: string | string[];
+  ingredients: Ingredient[];
+  steps: string[];
+  isFavourite?: boolean;
+  rating: number;
+  comments?: Comment[];
+  createdAt: string;
+}
+
+export interface Ingredient {
+  name: string;
+  amount: number;
+  unit: string;
 }
 
 export interface Comment {
