07cbc984cbf19d90e0c7f801273c7f80042ab3de
Ngoc Khanh <ngockhanh7643@gmail.com>
2025-09-07 12:33:03 +0700

    Update index.tsx

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,7 +1,3 @@
-import Button from "../../components/Button";
-import Input from "../../components/Input";
-import ReviewCard from "../../components/Review/ReviewCard";
-
 export default function LandingPage() {
   return (
     <>
@@ -14,13 +10,13 @@ export default function LandingPage() {
           className="absolute inset-0 w-full h-full object-cover -z-10"
         />
 
-        <div className="absolute inset-0 bg-white/30 -z-10"></div>
+        <div className="absolute inset-0 bg-white/10 -z-10"></div>
         {/* Content (aligned right) */}
-        <div className="sm:pl-40 font-worksans w-full md:w-1/2 text-left sm:text-left relative z-10">
+        <div className="sm:pl-40 font-worksans w-full md:w-1/2 text-right sm:text-left relative z-10">
           <h2 className="text-2xl md:text-4xl font-lobster text-primary mb-4">
             Nom Nom
           </h2>
-          <p className="text-lg md:text-2xl text-white font-semibold mb-6">
+          <p className="text-lg md:text-2xl text-black sm:text-white font-semibold mb-6">
             Find recipes you’ll love, <br /> with the ease you need.
           </p>
           <button className="text-md md:text-xl bg-primary font-playfair text-white px-6 py-3 rounded-full font-bold hover:bg-[#732c4e] transition">
