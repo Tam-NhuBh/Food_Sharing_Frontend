ec3885ce5ed7aecabc23b1607b8806d3a9641f46
Ngoc Khanh <ngockhanh7643@gmail.com>
2025-09-19 00:32:32 +0700

    fixing error deploying

--- src/components/CategoryFilter/index.tsx
+++ src/components/CategoryFilter/index.tsx
@@ -1,4 +1,4 @@
-import { ChevronLeft, ChevronRight, Filter } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import type { Category } from "../../types";
 import Button from "../Button";
 import { useRef } from "react";
--- src/contexts/useTheme.tsx
+++
@@ -1,7 +0,0 @@
-export const useTheme = () => {
-  const context = React.useContext(ThemeContext);
-  if (!context) {
-    throw new Error('useTheme must be used within a ThemeProvider');
-  }
-  return context;
-};
